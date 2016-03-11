3e662e689a37d573cbc4ce7004d5b1d568c44566
Thomas Riechert <thomas.riechert@htwk-leipzig.de>
2016-03-11 09:40:55 +0100

    add ontowiki

---
+++ docker/ontowiki/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CONFIG_ONTOWIKI=/var/www/config.ini
+LOG_ONTOWIKI="/var/www/logs/ontowiki.log"
+
+# execute config script
+chmod +x /config/config-run.sh
+/config/config-run.sh
+
+# set Virtuoso password in ontowikis config.ini
+sed -i "s/\(store.virtuoso.password\s*\)= \"dba\"$/\1= \"${STORE_ENV_PWDDBA}\"/" ${CONFIG_ONTOWIKI}
+
+# start the php5-fpm service
+echo "starting php …"
+service php5-fpm start
+
+# start the nginx service
+echo "starting nginx …"
+service nginx start
+
+echo "OntoWiki is ready to set sail!"
+cat /ow-docker.fig
+echo ""
+echo "following log:"
+
+touch $LOG_ONTOWIKI
+chmod a+w $LOG_ONTOWIKI
+tail -f $LOG_ONTOWIKI
