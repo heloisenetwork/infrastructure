4d4743a77421ad90b35f04bf38bdfe7e3c7ae872
Tobias Hahn <hahn.tobias66@gmail.com>
2016-05-19 12:48:40 +0200

    added docer dir for project-display

---
+++ docker/project-display/run.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# change this line to rewrite docker-cache
+
+# execute config script
+#chmod +x /config/config-run.sh
+#/config/config-run.sh
+
+# give php write access to data dir
+#chown www-data /var/www/data
+
+#cd /var/www/data/
+#ln -s ../models/cpm_2015.n3
+
+# start the php5-fpm service
+#echo "starting php …"
+#service php5-fpm start
+
+# start the nginx service
+echo "starting nginx …"
+service nginx start
+
+#echo "Here is the VocTo...!"
+#echo ""
+#echo "following log:"
+
+#LOG="/var/www/voctologs.log"
+#touch $LOG
+#chmod a+w $LOG
+touch test.txt
+tail -f test.txt
+exec bash
