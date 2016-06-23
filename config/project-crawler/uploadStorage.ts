9a359f2a9d34513bfa66d70d472dbfb49bc0bd59
Tobias Hahn <hahn.tobias66@gmail.com>
2016-06-23 21:16:42 +0200

    added upload-version

---
+++ config/project-crawler/uploadStorage.ts
@@ -0,0 +1,8 @@
+module Configuration{
+	
+	export class UploadConfiguration{
+		public static HELOISE_UPLOAD_DIR :string = "uploads/";
+	  public static FILE_NAME :string					= "heloise.csv";	
+	  public static REDIRECT_URL :string					= "http://localhost:80/uploader/web/index.html";	
+	}
+}
