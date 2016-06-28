3875eebf4cfebce9372e50bdc887893392b9d7ba
Tobias Hahn <hahn.tobias66@gmail.com>
2016-06-28 09:50:09 +0200

    modifed sample uploadStorage.ts

--- config/project-crawler/uploadStorage.ts
+++ config/project-crawler/uploadStorage.ts
@@ -3,6 +3,6 @@ module Configuration{
 	export class UploadConfiguration{
 		public static HELOISE_UPLOAD_DIR :string = "uploads/";
 	  public static FILE_NAME :string					= "heloise.csv";	
-	  public static REDIRECT_URL :string					= "http://localhost:80/uploader/web/index.html";	
+	  public static REDIRECT_URL :string					= "http://localhost:80/uploader/index.html";	
 	}
 }
